f286cf7145d87a58af64f94a4be79189497f4db0
Geequlim <geequlim@gmail.com>
2022-08-07 15:36:59 +0800

    Start build workflow after version updated [ci skip]

--- .github/workflows/version.js
+++ .github/workflows/version.js
@@ -1,27 +1,9 @@
 const request = require('request');
 const fs = require('fs');
-const { Octokit } = require("@octokit/core");
-const octokit = new Octokit({ auth: process.env.REPOSITORY_TOKEN });
-
+const child_process = require('child_process');
 
 async function updateV8(version) {
-  const workflows = ['android.yml', 'ios.yml', 'linux.yml', 'macos.yml', 'windows.yml'];
-  for (const workflow of workflows) {
-    const options = {
-      owner: 'Geequlim',
-      repo: 'v8-builder',
-      workflow_id: workflow,
-      ref: 'main',
-      inputs: { version }
-    };
-    try {
-      console.log('Start workflow', options.workflow_id);
-      await octokit.request('POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', options);
-    } catch (error) {
-      console.error(error);
-    }
-    console.log(JSON.stringify(options, undefined, '  '));
-  }
+  child_process.execSync(`echo "NEW_VERSION=${version}" >> $GITHUB_ENV`, { stdio: 'inherit' });
 }
 
 request({
