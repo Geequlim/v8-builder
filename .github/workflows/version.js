90ea49e0f1fc1c767807811acf5b67ddff595a26
Geequlim <geequlim@gmail.com>
2022-08-07 12:15:22 +0800

    update publish process

--- .github/workflows/version.js
+++ .github/workflows/version.js
@@ -1,32 +1,5 @@
 const request = require('request');
 const fs = require('fs');
-const { Octokit } = require("@octokit/core");
-const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
-
-async function updateV8(version, lastVersion) {
-  console.log(`Update v8 from ${lastVersion} to ${version}`);
-  const date = (new Date()).toISOString().split('T')[0].replace(/-/g, '');
-  for (const type of [ 'static', 'dynamic' ]) {
-    const action = {
-      owner: 'Geequlim',
-      repo: 'v8-builder',
-      workflow_id: 'build.yml',
-      ref: 'main',
-      inputs: {
-        type,
-        version,
-        tag: `nightly-${date}`
-      }
-    };
-    try {
-      await octokit.request('POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', action);
-      console.log('Start build action: ');
-    } catch (error) {
-      console.error("Request build action failed: ", error);
-    }
-    console.log(JSON.stringify(action, undefined, '  '));
-  }
-}
 
 request({
     'method': 'GET',
@@ -44,7 +17,7 @@ request({
     const versionFile = 'v8_version';
     const lastVersion = fs.readFileSync(versionFile, 'utf-8').trim();
     if (lastVersion != v8_version) {
+      console.log(`Update v8 from ${lastVersion} to ${version}`);
       fs.writeFileSync(versionFile, v8_version, 'utf-8');
-      updateV8(v8_version, lastVersion);
     }
 });
